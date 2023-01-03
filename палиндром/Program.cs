026fe5a5cd32244443d7a01e8ea2e359e23e3807
MerkulAlseitov <merkul1999@gmail.com>
2023-01-03 15:23:03 +0600

    Добавьте файлы проекта.

---
+++ палиндром/Program.cs
@@ -0,0 +1,25 @@
+﻿string word= Console.ReadLine().ToLower();
+char[] letters = word.ToArray();
+char[] numbers = word.ToArray();
+int a = 0;
+int b = numbers.Length;
+Array.Reverse(letters);
+foreach (var item in letters)
+{
+    Console.WriteLine(item);
+}
+for (int i = 0; i < letters.Length; i++)
+{
+    if (letters[i] == numbers[i])
+    {
+        a++;
+    }
+}
+if (a == b)
+{
+    Console.WriteLine("word is palindrome");
+}
+else
+{
+    Console.WriteLine("word is not palindrome");
+}
